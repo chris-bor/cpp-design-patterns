89efe2a6ed436e0280a743cc1f98b2155c395770
Krzysztof Borowiec <kr.borowiec@gmail.com>
2019-05-06 21:17:29 +0200

    Factory - square and circle classes.

--- Factory/figure.h
+++ Factory/figure.h
@@ -15,3 +15,27 @@ public:
     virtual void read(std::istream& is) = 0;
 };
 
+class Square : public Figure {
+public:
+    void write(std::ostream &os) const
+    {
+        os << static_cast<uint8_t>(Type::SQUARE);
+        // other members
+    }
+    virtual void read(std::istream& is)
+    {
+    }
+};
+
+class Circle : public Figure {
+public:
+    void write(std::ostream &os) const
+    {
+        os << static_cast<uint8_t>(Type::CIRCLE);
+        // other members
+    }
+    virtual void read(std::istream& is)
+    {
+    }
+};
+
