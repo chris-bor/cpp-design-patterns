e3a8b8c2bd301a6f154a770243b1e1ecfd7b48c7
Krzysztof Borowiec <kr.borowiec@gmail.com>
2019-06-16 20:56:32 +0200

    Add test weak pointers.
    If cicle exists using shared pointer is a wrong decision.

--- tests/tst_cpp_deliberates.cpp
+++ tests/tst_cpp_deliberates.cpp
@@ -1,6 +1,6 @@
-#include <iostream>>
 #include <list>
 #include <map>
+#include <memory>
 #include <vector>
 
 #include "tst_cpp_deliberates.h"
@@ -23,8 +23,55 @@ void CppDeliberates::test_list_memset()
     for(const auto& c : l)
         std::cout << c;
     std::cout << std::endl;
-//    std::memset(&l.front(), 0, 3); // commented because it can destroy sth else
+    //    std::memset(&l.front(), 0, 3); // commented because it can destroy sth else
     for(const auto& c : l)
         std::cout << c;
     std::cout << std::endl;
+}
+
+namespace weakpointer {
+void A::use()  {
+    if (mB.expired()) {
+        std::cout << "mB expired\n";
+        QVERIFY(mB.use_count() == 0);
+    }
+    else {
+        auto ptr = mB.lock();
+        std::cout << this->mName << " is using " << ptr->mName << std::endl;
+        std::cout << "mB use count: " << mB.use_count() << std::endl;
+        QVERIFY(mB.use_count() == 2);
+    }
+}
+
+void B::use()  {
+    if (mA.expired()) {
+        std::cout << "mA expired\n";
+        QVERIFY(mA.use_count() == 0);
+    }
+    else {
+        auto ptr = mA.lock();
+        std::cout << this->mName << " is using " << ptr->mName << std::endl;
+        std::cout << "mA use count: " << mA.use_count() << std::endl;
+        QVERIFY(mA.use_count() == 2);
+    }
+}
+}
+void CppDeliberates::test_weak_ptrs()
+{
+    using namespace weakpointer;
+    std::shared_ptr<A> a(new A);
+    a->use();
+    QVERIFY(a->mB.use_count() == 0);
+    {
+        std::shared_ptr<B> b(new B);
+        QVERIFY(b->mA.use_count() == 0);
+        a->set(b);
+        QVERIFY(a->mB.use_count() == 1);
+        b->set(a);
+        QVERIFY(b->mA.use_count() == 1);
+        a->use();
+        b->use();
+    }
+    QVERIFY(a->mB.use_count() == 0);
+    a->use();
 }
--- tests/tst_cpp_deliberates.h
+++ tests/tst_cpp_deliberates.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <QtTest>
 #include "tst_cpp_deliberates.h"
 
@@ -16,4 +17,24 @@ private slots:
     void cleanupTestCase(){}
     void test_vector_memset();
     void test_list_memset();
+    void test_weak_ptrs();
 };
+
+namespace weakpointer {
+struct B;
+
+struct A {
+    void set(std::weak_ptr<B> b) {mB = b;}
+    void use();
+    const std::string mName{"struct A"};
+    std::weak_ptr<B> mB;
+};
+
+struct B {
+    void set(std::weak_ptr<A> a) {mA = a;}
+    void use();
+    const std::string mName{"struct B"};
+    std::weak_ptr<A> mA;
+};
+
+} // namespace weakpointer
